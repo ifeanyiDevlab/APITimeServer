5adbefe3e10d83fed4ab0d08419cf6ad2a917ebf
ifeanyiDevlab <ifeanyionovo@outlook.com>
2024-11-14 17:37:47 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -8,6 +8,6 @@ version = "~> 4.0"
 }
 provider "aws" {
 region = "eu-north-1"
-AWS_k = "AKIA3ISBVNZ322M4CI5E "
-AWS S_key = "RBmERDj9Mm1+V3gOj+Y+72IGHVL6Tghpnak4zled "
+AWS Access Key= ""
+AWS Secret Access key = " "
 }
